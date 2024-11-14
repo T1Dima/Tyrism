6e4f353f86a8e795d1d3a67ab4daf44a97fc2474
u31508 <u31508@P31508>
2024-11-14 16:17:35 +1000

    Добавьте файлы проекта.

---
+++ Tyrism/AppliactionData/Tour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyrism.AppliactionData
+{
+    internal class Tour
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public List<Review> Reviews { get; set; }
+        public decimal TicketCount { get; internal set; }
+    }
+}
---
+++ Tyrism/AppliactionData/TourViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyrism.AppliactionData
+{
+    public class TourViewModel
+    {
+       
+        private bool _showOnlyActive;
+
+        public TourViewModel()
+        {
+            Tours = new ObservableCollection<Tour>(); // Примерный список туров
+            FilteredTours = new ObservableCollection<Tour>(Tours);
+            Types = new List<string> { "Все типы"};
+        }
+
+    
+        public List<string> Types { get; }
+
+        public decimal TotalPrice => FilteredTours.Sum(t => t.Price * t.TicketCount);
+
+    }
+}
